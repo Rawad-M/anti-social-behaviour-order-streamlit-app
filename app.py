1520049a33736c17a485cd34e8dff74e9e3144ec
Rawad-M <rawad.i.malik@gmail.com>
2025-04-21 12:42:57 +0100

    Read the data file from pandas

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 st.title("UK Government anti-social behaviour order (ASBO)")
 st.write("This application will visualise & display ASBO data for analysis and deeper understanding of the dataset")
 
 
+# Load dataset from /data folder
+data_asbo_issued = pd.read_csv('data/anti-social-behaviour-order-statistics-court-level-issued-0113.csv', encoding='ISO-8859-1')  
+data_asbo_breached = pd.read_csv('data/anti-social-behaviour-order-statistics-court-level-breaches-0113.csv', encoding='ISO-8859-1')  
+
+
+data_asbo_issued.info()
+data_asbo_breached.info()
