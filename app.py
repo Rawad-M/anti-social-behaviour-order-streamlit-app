bdf4160018856134f206ae30bc538dcfac173602
Rawad-M <rawad.i.malik@gmail.com>
2025-05-03 09:28:51 +0100

    added overview section

--- app.py
+++ app.py
@@ -9,11 +9,15 @@ st.write("This application will visualise & display ASBO data for analysis and d
 data_asbo_issued = pd.read_csv('data/anti-social-behaviour-order-statistics-court-level-issued-0113.csv', encoding='ISO-8859-1')  
 data_asbo_breached = pd.read_csv('data/anti-social-behaviour-order-statistics-court-level-breaches-0113.csv', encoding='ISO-8859-1')  
 
-#data_asbo_issued.info()
-#data_asbo_breached.info()
-
+# Create table overview
+st.subheader("Data Overview")
+st.write("Issued ASBOs dataset overview:")
+st.dataframe(data_asbo_issued.head())
+st.write("Breached ASBOs dataset overview:")
+st.dataframe(data_asbo_breached.head())
 
 # Plot
+st.subheader("Issued ASBOs Data Overview")
 fig, ax = plt.subplots()
 ax.bar(data_asbo_issued['Year of Issue'], data_asbo_issued['ASBOs issued'], color='skyblue')
 ax.set_xlabel("Year of Issue")
